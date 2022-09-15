97f901fa142d65c17e98824468d34e21cebe04af
Kevin <juwon8891@gmail.com>
2022-09-15 09:33:50 +0900

    secret file

---
+++ IdeaProjects/secret/data-h2.sql
@@ -0,0 +1,3 @@
+insert into member (id, address, disabled_number, email, password, phone_number, user_id, user_name)
+values (1, '테스트 주소', '테스트 넘버', 'test1@gmail.com', '테스트1의 본문', now(), now());
+insert into member (title, author, content, created_date, modified_date) values ('테스트2', 'test2@gmail.com', '테스트2의 본문', now(), now());
